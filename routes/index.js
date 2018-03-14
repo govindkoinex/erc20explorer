4d45b3d6235070f6fcde858138bc3988a233c659
Govind Vaid <govindvaid@Govinds-MacBook-Pro.local>
2018-03-14 19:29:07 +0530

    multiple coins on index page

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,37 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/:coinkey', function(req, res, next) {
+router.get('/:coinkey?', function(req, res, next) {
   var db = req.app.get('db');
   var config = req.app.get('config');
-  var coinkey = req.params.coinkey;
-  console.log("oivin",req.params);
-  
+  if (req.params.coinkey){
+    var coinkey = req.params.coinkey;
+    db[coinkey].find({}).sort({ timestamp: -1 }).limit(10).exec(function (err, events) {
 
-  db[coinkey].find({}).sort({ timestamp: -1 }).limit(10).exec(function (err, events) {
+      res.render('index', { events: events, coinkey:coinkey,config:config  });
+    });
+  }
+  else{
+    var fullevents={};
+    var querycount =0;
+    var keys=Object.keys(config.allCoins);
+    keys.map(function(elem) {
+      db[elem].find({}).sort({ timestamp: -1 }).limit(10).exec(function (err, events) {
+        fullevents[elem]=events;
+        // events.map(function(element){
+        //   element["coin"]=elem;
+        // });
+        // fullevents=fullevents.concat(events);
 
-    res.render('index', { events: events, coinkey:coinkey,config:config  });
-  });
+        querycount++;
+        if(querycount==keys.length)
+        {
+          console.log('all e',fullevents);
+          res.render('index', { events: fullevents, coinkey:'ALL TOKENS',config:config,coinArr:keys  });
+        }
+      });
+    });
+  }
 
 });
 
